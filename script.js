dbed874adf64a093a331cd6e441a58aa429b4c07
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2022-10-20 21:04:35 -0700

    added score counter

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var block = document.getElementById("block");
 var isPaused = false;
 var pause = document.getElementById("pause");
 var resume = document.getElementById("resume");
+var heading = document.querySelector("h1");
+var heading2 = document.querySelector("h2");
+var count = 0;
 
 window.addEventListener('keydown', function(e){
     if(e.key == "z"){
@@ -10,10 +13,9 @@ window.addEventListener('keydown', function(e){
     }
     if(e.key == "p"){
         pauseGame();
-        isPaused = true;
     }
     if(e.key == "r"){
-        isPaused = false;
+        
         startGame();
     }
 })
@@ -21,12 +23,16 @@ window.addEventListener('keydown', function(e){
 setTimeout(startGame, 1000);
 
 function startGame(){
+    heading2.innerText = "Score:" + count;
+    isPaused = false;
     block.classList.add("block-animate");
     resume.disabled = true;
-    pause.disabled = false; 
+    pause.disabled = false;
+    heading.innerText = "'Z' to jump!";
 }
 
 function pauseGame(){
+    isPaused = true;
     block.classList.remove("block-animate")
     pause.disabled = true;
     resume.disabled = false;
@@ -43,20 +49,28 @@ function jump(){
     
 }
 
-// registers collision and resets the game
+// registers collision, keeps count, and resets the game
 setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
     if(blockLeft < 100 && blockLeft > 80 && characterTop>=130){
-        alert("You lose.");
+        heading.innerText = "You died!"
         block.classList.remove("block-animate");
-        if (!isPaused) {
-            setTimeout(startGame, 500);
-        }
-    }
-    if(isPaused){
         pauseGame();
     }
 }, 10)
+
+// keeps score based on how many seconds survived
+setInterval(function(){
+    if(!isPaused){
+        count++;
+        heading2.innerText = "Score:" + count;
+    }
+    else{
+        count = 0;
+    }
+    
+}, 1000)
 
 
