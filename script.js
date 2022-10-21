b5bed59d0666dbd90058eea886832d3d51d07719
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2022-10-20 18:01:00 -0700

    added pause & resume buttons

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ window.addEventListener('keydown', function(e){
     }
 })
 
-setTimeout(function(){
-    block.classList.add("block-animate");
-}, 1000);
+// delays start of the game
+// setTimeout(function(){
+//     block.classList.add("block-animate");
+// }, 1000);
 
+setTimeout(startGame, 1000);
+
+function startGame(){
+    block.classList.add("block-animate");
+}
+
+function pauseGame(){
+    block.classList.remove("block-animate")
+}
+
+// makes the character jump
 function jump(){
     if(character.classList != "animate"){
         character.classList.add("animate");
@@ -21,15 +33,15 @@ function jump(){
     
 }
 
+// registers collision and resets the game
 setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft < 100 && blockLeft > 80 && characterTop>=130){
         alert("You lose.");
         block.classList.remove("block-animate");
-        setTimeout(function(){
-            block.classList.add("block-animate");
-        }, 1000);
+        setTimeout(startGame, 1000);
     }
 }, 10)
+
 
